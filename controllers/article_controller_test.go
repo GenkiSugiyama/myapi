d689ca4dd8abe923908454f00a763e3973a13d2c
杉山 元基 <genki-sugiyama@sugiyamanoMacBook-Air.local>
2025-12-11 00:49:40 +0900

    add: [done]implement unit tests of controllers

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestArticleListHandler(t *testing.T) {
@@ -29,6 +31,36 @@ func TestArticleListHandler(t *testing.T) {
 			res := httptest.NewRecorder()
 
 			aCon.ArticleListHandler(res, req)
+
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	var tests = []struct {
+		name       string
+		articleID  string
+		resultCode int
+	}{
+		{name: "number pathparam", articleID: "1", resultCode: http.StatusOK},
+		{name: "alphabet pathparam", articleID: "aaa", resultCode: http.StatusNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.articleID)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+			res := httptest.NewRecorder()
+
+			// ArticleDetailHandlerはmux.Vars()を使用してパスパラメータを取得しているため、
+			// テストコード内でパスパラメータを正しく取得できるようにするためにmux.Routerを使用してハンドラを呼び出す必要がある
+			// 実際のハンドラ登録と同様にmux.Routerを作成し、パスパラメータ付きのURLパターンでハンドラを登録する
+			r := mux.NewRouter()
+			r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
+			r.ServeHTTP(res, req)
 
 			if res.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
--- controllers/main_test.go
+++ controllers/main_test.go
@@ -1,14 +1,10 @@
 package controllers_test
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/GenkiSugiyama/myapi/controllers"
-	"github.com/GenkiSugiyama/myapi/services"
+	"github.com/GenkiSugiyama/myapi/controllers/testdata"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,18 +12,7 @@ import (
 var aCon *controllers.ArticleController
 
 func TestMain(m *testing.M) {
-	dbUser := "user"
-	dbPassword := "pass"
-	dbDatabase := "myapi_db"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Printf("DB setup failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	ser := services.NewMyAppService(db)
+	ser := testdata.NewServiceMock()
 	aCon = controllers.NewArticleController(ser)
 
 	m.Run()
